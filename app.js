a11354410bcd5cd3cf6d82c3f261fed06cc09d84
Erwin Martinez <erwinmart17m@gmail.com>
2026-03-29 23:26:15 -0600

    Prometheus con Grafana

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ var path = require('path')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
 
+const client = require('prom-client') // Cliente de Prometheus
+
 var indexRouter = require('./src/routes/index')
 var usersRouter = require('./src/routes/users')
 var itemsRouter = require('./src/routes/items')
 
 var app = express()
+
+client.collectDefaultMetrics()
 
 app.use(logger('dev'))
 app.use(express.json())
@@ -15,8 +19,38 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
+const httpRequestCounter = new client.Counter({
+  name: 'http_requests_total',
+  help: 'Total de peticiones HTTP procesadas',
+  labelNames: ['metodo', 'ruta', 'estado_http']
+})
+
+const activeUsersGauge = new client.Gauge({
+  name: 'active_users_current',
+  help: 'Número actual de usuarios activos simulados'
+})
+
+app.use((req, res, next) => {
+  res.on('finish', () => {
+    httpRequestCounter
+      .labels(req.method, req.route ? req.route.path : req.path, res.statusCode)
+      .inc()
+  })
+  next()
+})
+
+setInterval(() => {
+  const simulatedUsers = Math.floor(Math.random() * 100)
+  activeUsersGauge.set(simulatedUsers)
+}, 5000)
+
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
 app.use('/items', itemsRouter)
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType)
+  res.send(await client.register.metrics())
+})
 
 module.exports = app
