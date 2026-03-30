ce17d9c0cfa8e9701f7728266f2b56bb7d5c69ec
Erwin Martinez <erwinmart17m@gmail.com>
2026-03-29 23:51:01 -0600

    Observabilidad con prometheus y grafana v1

--- app.js
+++ app.js
@@ -11,8 +11,6 @@ var itemsRouter = require('./src/routes/items')
 
 var app = express()
 
-client.collectDefaultMetrics()
-
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -20,34 +18,34 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 const httpRequestCounter = new client.Counter({
-  name: 'http_requests_total',
-  help: 'Total de peticiones HTTP procesadas',
-  labelNames: ['metodo', 'ruta', 'estado_http']
+    name: 'http_requests_total',
+    help: 'Total de peticiones HTTP procesadas',
+    labelNames: ['metodo', 'ruta', 'estado_http']
 })
 
 const activeUsersGauge = new client.Gauge({
-  name: 'active_users_current',
-  help: 'Número actual de usuarios activos simulados'
+    name: 'active_users_current',
+    help: 'Número actual de usuarios activos simulados'
 })
 
 app.use((req, res, next) => {
-  res.on('finish', () => {
-    httpRequestCounter
-      .labels(req.method, req.route ? req.route.path : req.path, res.statusCode)
-      .inc()
-  })
-  next()
+    res.on('finish', () => {
+        httpRequestCounter
+            .labels(req.method, req.route ? req.route.path : req.path, res.statusCode)
+            .inc()
+    })
+    next()
 })
 
 const isTest = process.env.NODE_ENV === 'test'
 
 if (!isTest) {
-  client.collectDefaultMetrics()
+    client.collectDefaultMetrics()
 
-  setInterval(() => {
-    const simulatedUsers = Math.floor(Math.random() * 100)
-    activeUsersGauge.set(simulatedUsers)
-  }, 5000)
+    setInterval(() => {
+        const simulatedUsers = Math.floor(Math.random() * 100)
+        activeUsersGauge.set(simulatedUsers)
+    }, 5000)
 }
 
 app.use('/', indexRouter)
@@ -55,8 +53,8 @@ app.use('/users', usersRouter)
 app.use('/items', itemsRouter)
 
 app.get('/metrics', async (req, res) => {
-  res.set('Content-Type', client.register.contentType)
-  res.send(await client.register.metrics())
+    res.set('Content-Type', client.register.contentType)
+    res.send(await client.register.metrics())
 })
 
 module.exports = app
